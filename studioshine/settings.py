edd24e5c0aa72384af92c021765e31cd9e554780
Dinesh S Piyasamara <64399401+dineshpiyasamara@users.noreply.github.com>
2022-10-12 15:55:37 +0530

    Update settings.py

--- studioshine/settings.py
+++ studioshine/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$biv)b-pph@+8l9x0&=9x&gj@%rjs%enqq!c!4#l5giymab%2d'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
