049a3a54f497cfe6d49408e50535fa182689590c
dinesh <silvershine.zero@gmail.com>
2022-10-13 06:09:33 +0530

    hide key

--- studioshine/settings.py
+++ studioshine/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$biv)b-pph@+8l9x0&=9x&gj@%rjs%enqq!c!4#l5giymab%2d'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -78,9 +79,9 @@ WSGI_APPLICATION = 'studioshine.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'studioshine',
-        'USER': 'root',
-        'PASSWORD': 'codeprolk',
+        'NAME': config('DBname'),
+        'USER': config('DBuser'),
+        'PASSWORD': config('DBpassword'),
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
